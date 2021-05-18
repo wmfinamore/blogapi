940d024b1a820e7598f24ada5df7aecf8985b9ee
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-05-17 22:14:19 -0300

    Included route for documentation api using swagger

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.schemas import get_schema_view
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.infor(
+        title="Blog API",
+        default_version="v1",
+        description="A simple API for learning DRF",
+        terms_of_service="https://www.google.com/policies/com",
+        contatc=openapi.Contact(email="hello@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 
 urlpatterns = [
@@ -25,9 +41,8 @@ urlpatterns = [
     path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')),
     path('api/v1/dj-rest-auth/registration/',
          include('dj_rest_auth.registration.urls')),
-    path('openapi', get_schema_view(
-        title='Blog API',
-        description='A sample API for learning DRF',
-        version="1.0.0"
-    ), name='openapi-schema'),
+    path('swagger/', schema_view.with_ui(
+        'swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui(
+        'redoc', cache_timeout=0), name='schema_redock'),
 ]
